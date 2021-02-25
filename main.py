edb3c95e2af6a0116aadb9402dc412f7c83a24f2
Josh Weintraub <jhweintraub@gmail.com>
2021-02-25 00:48:08 -0500

    Refactored code so that now price is a heroku config var rather than an environment variable. This is because the heroku scheduler runs the script as a literal python command so we need the config var to retain its changed value from run to run. Anything else and it would stay the same so once your price gets hit you're gonna get pinged every ten minutes about it forever until you manually change it in heroku. This fixes it automatically.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from coinbase.wallet.client import Client as CoinbaseClient
 from binance.client import Client as binanceClient
 from twilio.rest import Client as twilioClient
+import heroku3
 
 
 def get_coinbase_price():
@@ -13,6 +14,7 @@ def get_coinbase_price():
 
     ether_price = client.get_buy_price(currency_pair='ETH-USD').amount
     return {"exchange": "Coinbase", "price": float(ether_price)}
+
 
 def get_binance_price():
     binance_key = os.getenv("binance_api_key")
@@ -40,11 +42,16 @@ def send_price_alert(exchange, price, isDrop):
         from_=os.getenv("twilio_phone_number"),
         body=message)
 
+
 def main():
     load_dotenv()
 
-    low_alert_amount = float(os.getenv("low_alert_amount"))
-    high_alert_amount = float(os.getenv("high_alert_amount"))
+    cloud = heroku3.from_key(os.getenv("heroku_api_key"))
+    app = cloud.apps()['cryptocurrency-notifications']
+    config = app.config()
+
+    low_alert_amount = float(config['low_alert_amount'])
+    high_alert_amount = float(config['high_alert_amount'])
 
     prices = []
     get_binance_price()
@@ -53,17 +60,18 @@ def main():
     print(prices)
 
     for x in prices:
-        if(x["price"]) > low_alert_amount:
+        if (x["price"]) > low_alert_amount:
             # send_price_alert(x["exchange"], x["price"], True)
-            print("this is a test")
-            os.environ['low_alert_amount'] = str(low_alert_amount - 100)
-            # print(float(os.getenv("low_alert_amount")))
+            # print("this is a test")
+            print(config['low_alert_amount'])
+            config['low_alert_amount'] = str(low_alert_amount - 100.00)
             # so you don't get spammed by every exchange but do get alerted by sudden crashed
             break
 
         if (x["price"]) > high_alert_amount:
             send_price_alert(x["exchange"], x["price"], False)
-            os.environ['high_alert_amount'] = str(high_alert_amount + 100)
+            config['high_alert_amount'] = str(high_alert_amount + 100.00)
             break
+
 
 main()
